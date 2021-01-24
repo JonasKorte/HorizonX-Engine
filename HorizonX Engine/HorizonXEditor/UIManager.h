06101530123326fda11b82477e19445dc397837e
JonasKorte <jonaskorte2904@gmail.com>
2021-01-24 22:10:29 +0100

    Updated UIManager

--- HorizonX Engine/HorizonXEditor/UIManager.h
+++ HorizonX Engine/HorizonXEditor/UIManager.h
@@ -1,19 +1,39 @@
 #pragma once
+#include <map>
 
 namespace HXEditor
 {
+	typedef const char* HX_ID;
+
 	struct HX_Component
 	{
-		const char* id;
+		HX_ID id;
 		
 		void* Load();
 		void* Unload();
 		void* Update();
+
+		HX_ID parent;
 	};
+
+	typedef std::map<HX_ID, HX_Component> HX_ComponentCollection;
+
 
 	class HX_UIManager
 	{
 	public:
-		static HX_UIManager* GetInstance();
+		static void RegisterComponent(HX_Component component)
+		{
+			HX_ID id = "0x" + cast<HX_ID>(m_numComponents);
+
+			m_numComponents++;
+		}
+
+	private:
+		static std::map<HX_ComponentCollection, bool> m_components;
+
+		static int m_numComponents;
+
+		static HX_UIManager* m_instance;
 	};
 }
